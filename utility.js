8525946a79361274290401d9ed6a10d1469291e7
patipan <patipan63@nu.ac.th>
2022-08-29 16:27:43 +0700

    codelab6

--- utility.js
+++ utility.js
@@ -2,42 +2,37 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MDk4NjA1LCJleHAiOjE2NTkxMDIyMDUsIm5iZiI6MTY1OTA5ODYwNSwianRpIjoiYWcyZWFIeGM4MHFrNU0wcCIsInN1YiI6MzMsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.QSMRuSLYa6OmwELcaT12U8Jwy9c2nd4i7oVy9zpgO9Q"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzY0NjY4LCJleHAiOjE2NjE3NjgyNjgsIm5iZiI6MTY2MTc2NDY2OCwianRpIjoic01ScmtoZ2R2RUFERHdMYiIsInN1YiI6NTcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Nkqo6oYbTzkR7FdW10w_FmuPRtZzG85hI2hlgT16Ey4"; // put access token
 const student = {
-    name: '', 
-    age: 20, 
-    gender: '', 
-    department: 'CPE'
-  };
+  id_student:63367230,
+  name: 'patipan', // replace with your full name.
+  age: 20, // put your age.
+  gender: 'Male', // replace with your gender
+  department: 'CPE'
+};
 
 
-  exports.findStudentbyId = function (student_id, cb) {
-    //-- call external api 
-            request({
-              method: "GET",
-              uri: EXTERNAL_API+student_id,
-              headers: {
-                  Authorization: `Bearer ${accessToken}`
-              }
-          }).then((response) => {
-              console.log('Sent');
-              console.log({
-                  "request": student_id,
-                  "response": response
-              });
-  
-             cb(response);
-  
-          }).catch((err) => {
-              console.log('Error:', err.message);
-              console.log({
-                  "Error": err.message
-              });
-          });
-    }
-  
+exports.findStudentbyId = function (student_id, cb) {
+  //-- call external api 
+          request({
+            method: "GET",
+            uri: EXTERNAL_API+student_id,
+            headers: {
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzY0NjY4LCJleHAiOjE2NjE3NjgyNjgsIm5iZiI6MTY2MTc2NDY2OCwianRpIjoic01ScmtoZ2R2RUFERHdMYiIsInN1YiI6NTcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Nkqo6oYbTzkR7FdW10w_FmuPRtZzG85hI2hlgT16Ey4`
+            }
+        }).then((response) => {
+            console.log('Sent');
+            console.log({
+                "request": student_id,
+                "response": response
+            });
 
-exports.fakeStudentbyInfo = function (student_id, cb) {
+           cb(response);
 
-  cb(student);
+        }).catch((err) => {
+            console.log('Error:', err.message);
+            console.log({
+                "Error": err.message
+            });
+        });
 }
